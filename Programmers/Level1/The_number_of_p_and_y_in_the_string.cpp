5674124e4019307ff2b63626e3f899fc106bea63
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-03-05 20:08:53 +0900

    The_number_of_p_and_y_in_the_string.cpp

---
+++ Programmers/Level1/The_number_of_p_and_y_in_the_string.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+
+    int count_p = 0, count_y = 0;
+
+    for(int i = 0; i < s.length(); i++){
+        switch(s[i]){
+            case 'p':
+                count_p++;
+            break;
+            case 'P':
+                count_p++;
+            break;
+            case 'y':
+                count_y++;
+            break;
+            case 'Y':
+                count_y++;
+            break;
+        }
+    }
+
+    if(count_p == count_y)
+        return true;
+    else
+        return false;
+}
