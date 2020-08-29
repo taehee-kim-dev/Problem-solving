2d2c5e5b664eb4a31a4ae51c75165057da403cd1
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-08-30 07:47:36 +0900

    Fix comments of Programmers/Kakao/Level4/2020_KAKAO_BLIND_RECRUITMENT/Lyrics_search.py

--- Programmers/Kakao/Level4/2020_KAKAO_BLIND_RECRUITMENT/Lyrics_search.py
+++ Programmers/Kakao/Level4/2020_KAKAO_BLIND_RECRUITMENT/Lyrics_search.py
@@ -12,7 +12,7 @@ class Node:
         self.char = char
         # 자식노드들
         self.child_char_nodes = dict()
-        # 이 노드를 기준으로 자식노드들이 각 깊이별로 몇개씩 있는지
+        # 이 노드를 기준으로 남은 글자길이별로 단어들이 몇개씩 존재하는지
         self.number_of_remain_length_from_this_node = dict()
 
 
@@ -23,32 +23,32 @@ class Trie:
 
     # 노드 추가
     def insert_node(self, word):
-        # 현재 검사중인 노드
-        current_node = self.root_node
-        # 현재 추가하고있는 글자 이후로 남아있는 글자의 개수
+        # 현재 추가하려는 글자의 부모노드
+        parent_node = self.root_node
+        # 현재 추가하려는 글자를 포함한 나머지 글자의 개수
         remaining_length = len(word)
         # 단어에서 한 글자씩 차례대로 꺼내서 추가
         for char in word:
-            if remaining_length in current_node.number_of_remain_length_from_this_node:
+            if remaining_length in parent_node.number_of_remain_length_from_this_node:
                 """
-                현재 추가하고있는 글자 이후로 남아있는 글자의 개수가
-                현재 사용중인 노드의 number_of_remain_length_from_this_node에 key로 존재하면
+                현재 추가하려는 글자를 포함한 나머지 글자의 개수가
+                현재 추가하려는 글자노드의 부모노드의 남은 글자길이별 단어의 갯수에 key로 존재하면
                 해당 key값의 value를 1 증가
                 """
-                current_node.number_of_remain_length_from_this_node[remaining_length] += 1
+                parent_node.number_of_remain_length_from_this_node[remaining_length] += 1
             else:
                 """
                 존재하지 않는다면 초기값 1로 새로 추가
                 """
-                current_node.number_of_remain_length_from_this_node[remaining_length] = 1
+                parent_node.number_of_remain_length_from_this_node[remaining_length] = 1
 
-            if char not in current_node.child_char_nodes:
-                # 현재 추가중인 글자의 노드가 현재 검사중인 노드의 자식노드에 없다면
+            if char not in parent_node.child_char_nodes:
+                # 현재 추가하려는 글자의 노드가 부모노드의 자식노드중에 없다면
                 # 새로 만들어서 자식노드에 추가
-                current_node.child_char_nodes[char] = Node(current_node, char)
+                parent_node.child_char_nodes[char] = Node(parent_node, char)
 
-            # 현재 노드를 현재 추가한 글자의 노드로 변환 (한 단계 깊이 내려감)
-            current_node = current_node.child_char_nodes[char]
+            # 부모노드를 현재 추가한 글자의 노드로 변환 (한 단계 깊이 내려감)
+            parent_node = parent_node.child_char_nodes[char]
             # 남아있는 글자의 개수 1 감소
             remaining_length -= 1
 
@@ -70,13 +70,10 @@ class Trie:
                 # 없다면 검색결과는 0개
                 return 0
 
-        """
-        순수 단어의 글자들을 모두 검색한 이후
-        물음표 검사 차례
-        """
+        # 순수 단어의 글자들을 모두 검색한 이후 물음표 검사 차례
         if number_of_question_mark in current_node.number_of_remain_length_from_this_node:
             """
-            물음표의 개수가 현재 검사중인 노드의 number_of_remain_length_from_this_node의 key값으로 존재하면
+            남은 물음표의 개수가 현재 검사중인 노드의 number_of_remain_length_from_this_node의 key값으로 존재하면
             해당 key값에 대한 value값이 검색 결과
             """
             return current_node.number_of_remain_length_from_this_node[number_of_question_mark]
