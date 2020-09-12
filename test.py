37f7251a3f7b4d247cb02aef69c276048e3b655a
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-09-12 22:39:51 +0900

    Set for 2021 line-plus recruitment coding test

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-test = 'cde'
-test.sort()
+from collections import defaultdict
 
-print(test)
+test_dict = defaultdict(list)
+
+test_dict[0] = 'hi'
+test_dict[2] = 'hello'
+print(test_dict[5])
+
+print('tset')
+
+print(0 in test_dict)
+print(2 in test_dict)
+print(4 in test_dict)
+print(3 in test_dict)
