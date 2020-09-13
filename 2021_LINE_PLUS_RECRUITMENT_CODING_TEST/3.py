4d8bfa6f72c2b34970ce202de2282617c7fc7486
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-09-13 12:59:53 +0900

    Finish 2021_LINE_PLUS_RECRUITMENT_CODING_TEST

--- 2021_LINE_PLUS_RECRUITMENT_CODING_TEST/3.py
+++ 2021_LINE_PLUS_RECRUITMENT_CODING_TEST/3.py
@@ -1,9 +1,41 @@
 
 
 
+from queue import PriorityQueue
+
+def solution(n):
+    answer = []
+
+    if len(str(n)) == 1:
+        return [0, n]
+
+    priority_queue = PriorityQueue()
+    # [횟수, 숫자]
+    n = str(n)
+
+    for index, num_char in enumerate(n):
+        if index < len(n) - 1 and n[index + 1] != '0':
+            first = n[:index + 1]
+            second = n[index + 1:]
+            s = int(first) + int(second)
+            priority_queue.put([1, str(s)])
 
 
+    while not priority_queue.empty():
+        num_inform = priority_queue.get()
+        if len(num_inform[1]) == 1:
+            answer = [num_inform[0], int(num_inform[1])]
+            return answer
 
+        for index, num_char in enumerate(num_inform[1]):
+            if index < len(num_inform[1]) - 1 and num_inform[1][index + 1] != '0':
+                first = num_inform[1][:index + 1]
+                second = num_inform[1][index + 1:]
+                s = int(first) + int(second)
+                priority_queue.put([num_inform[0] + 1, str(s)])
+
+
+    return answer
 
 
 
@@ -13,6 +45,16 @@
 
 
 print(solution(
-
+73425
 ))
-#
+# [4, 3]
+
+print(solution(
+10007
+))
+# [4, 8]
+
+print(solution(
+9
+))
+# [0, 9]
--- 2021_LINE_PLUS_RECRUITMENT_CODING_TEST/4.py
+++ 2021_LINE_PLUS_RECRUITMENT_CODING_TEST/4.py
@@ -1,18 +1,103 @@
+import sys
+sys.setrecursionlimit(10*8)
 
 
+UP = (-1, 0)
+RIGHT = (0, 1)
+DOWN = (1, 0)
+LEFT = (0, -1)
 
 
+def find_next_left(direction, position):
+    if direction == LEFT:
+        return [(position[0] + DOWN[0], position[1] + DOWN[1]), DOWN]
+    elif direction == RIGHT:
+        return [(position[0] + UP[0], position[1] + UP[1]), UP]
+    elif direction == UP:
+        return [(position[0] + LEFT[0], position[1] + LEFT[1]), LEFT]
+    else:
+        return [(position[0] + RIGHT[0], position[1] + RIGHT[1]), RIGHT]
 
 
+def find_next_right(direction, position):
+    if direction == LEFT:
+        return [(position[0] + UP[0], position[1] + UP[1]), UP]
+    elif direction == RIGHT:
+        return [(position[0] + DOWN[0], position[1] + DOWN[1]), DOWN]
+    elif direction == UP:
+        return [(position[0] + RIGHT[0], position[1] + RIGHT[1]), RIGHT]
+    else:
+        return [(position[0] + LEFT[0], position[1] + LEFT[1]), LEFT]
 
 
+found = False
+total_sec = 0
 
 
+def find_next_back(direction):
+    if direction == LEFT:
+        return RIGHT
+    elif direction == RIGHT:
+        return LEFT
+    elif direction == UP:
+        return DOWN
+    else:
+        return UP
 
+
+def find_way(direction, second, position, N, maze):
+    global found, total_sec
+    if position == (N - 1, N - 1):
+        found = True
+        total_sec = second
+        return
+    next_left = find_next_left(direction, position)
+    next_straight = [(position[0] + direction[0], position[1] + direction[1]), direction]
+    next_right = find_next_right(direction, position)
+    next_back = [(position[0] - direction[0], position[1] - direction[1]), find_next_back(direction)]
+
+    for next_position in [next_left, next_straight, next_right, next_back]:
+        if 0 <= next_position[0][0] <= N - 1 and 0 <= next_position[0][1] <= N - 1 \
+                and maze[next_position[0][0]][next_position[0][1]] == 0:
+            if found:
+                return
+            find_way(next_position[1], second + 1, next_position[0], N, maze)
+
+
+def solution(maze):
+    answer = 0
+
+    if maze[0][1] == 0:
+        # 방향, 초, 현재위치
+        find_way(RIGHT, 0, (0, 0), len(maze), maze)
+    else:
+        find_way(DOWN, 0, (0, 0), len(maze), maze)
+
+    return total_sec
 
 
 
 print(solution(
-
+[
+    [0, 1, 0, 1],
+    [0, 1, 0, 0],
+    [0, 0, 0, 0],
+    [1, 0, 1, 0]
+]
 ))
-#
+# 10
+
+print(solution(
+[[0, 1, 0, 0, 0, 0], [0, 1, 0, 1, 1, 0], [0, 1, 0, 0, 1, 0], [0, 1, 1, 1, 1, 0], [0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 0]]
+))
+# 32
+
+print(solution(
+[[0, 1, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0]]
+))
+# 24
+
+print(solution(
+[[0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 1, 1], [0, 0, 0, 0, 0, 0], [1, 0, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0], [1, 1, 0, 1, 1, 0]]
+))
+# 22
